7de506c046cfe3ece6224fa014d07293f72d525e
Haruaki Kobayashi <rigelpain@gmail.com>
2021-06-13 16:24:05 +0900

    [fix] 冗長廃止

--- components/sp/Hero.vue
+++ components/sp/Hero.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="hero">
     <v-container class="pa-0">
-        <!-- white--text:文字白 alignend：下部表示 text-center: pタグ文字中央表示 -->
-        <!-- $vuetify.breakpoint.XXXXX: レスポンシブ対応。画面サイズによってクラス出し分け -->            
-        <!-- "/image/my-portfolio-top.jpg"がstatic以下に配置した画面へのパス -->            
-        <v-responsive :aspect-ratio="16/9">
-          <div :class="$style.BackStudy" background="primary">
-          <v-responsive :aspect-ratio="1/1">
-            <v-img
-              class="align-end text-center"
-              src="/images/SpHero.svg"
-              contain
-            >
-            </v-img>
-          </v-responsive>
-          </div>
+      <!-- white--text:文字白 alignend：下部表示 text-center: pタグ文字中央表示 -->
+      <!-- $vuetify.breakpoint.XXXXX: レスポンシブ対応。画面サイズによってクラス出し分け -->
+      <!-- "/image/my-portfolio-top.jpg"がstatic以下に配置した画面へのパス -->
+      <div :class="$style.BackStudy" background="primary">
+        <v-responsive :aspect-ratio="1/1">
+          <v-img
+            src="/images/SpHero.svg"
+            contain
+          >
+          </v-img>
         </v-responsive>
-
+      </div>
     </v-container>
   </div>
 </template>
